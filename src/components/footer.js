a72eef05785c56589c946480aa07b7e94c4f5b91
piharpi <justharpi@gmail.com>
2020-03-31 23:01:27 +0700

    sub-components for footer

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => (
+  <footer>
+    &copy; {new Date().getFullYear()}, Built with
+    {` `}
+    <a href="https://www.banyuwangidev.org">Banyuwangi Dev</a>
+  </footer>
+)
+
+export default Footer
