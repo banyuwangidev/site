ba56fb7043f342303f8cb3c3cce1081762f65345
fahmi irsyad khairi <fahmiirsyad10@gmail.com>
2020-04-02 11:30:46 +0700

    Fix responsive navigation item

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -23,23 +23,37 @@ const Nav = styled.nav`
 const NavItemLeft = styled.div`
     display: flex;
     align-items: center;
+    line-height: 1.2;
+    font-size: 14px;
+    height: 100%;
+    flex-grow: 0;
+    min-width: 0px;
 `
 
 const NavItemRight = styled.div`
-    display: flex;
+    user-select: none;
+    transition: background 120ms ease-in 0s;
+    cursor: pointer;
+    display: inline-flex;
     align-items: center;
+    flex-shrink: 0;
+    white-space: nowrap;
+    height: 28px;
+    border-radius: 3px;
+    font-size: 14px;
+    line-height: 1.2;
+    min-width: 0px;
     padding: 4px 0;
 `
 
 const LinkHolder = styled.div`
     border-radius: 6px;
-    line-height: 32px;
+    min-width: 0;
     padding: 4px 0;
     transition: background-color .2s ease;
     a {
         display: flex;
         align-items: center;
-        line-height: 32px;
         padding: 0 8px;
     }
     &:hover {
@@ -66,6 +80,18 @@ const SpanText = styled.span`
     padding: 0 8px;
 `
 
+const TextOverflow = styled.div`
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-width: ${props => props.size}
+`
+
+const FlexSeperate = styled.div`
+    flex-grow: 1;
+    flex-shrink: 1;
+`
+
 const Header = ({ title, crumbs }) => {
     const crumbsLen = crumbs.length === undefined ? 0 : crumbs.length;
     
@@ -75,7 +101,9 @@ const Header = ({ title, crumbs }) => {
                 <LinkHolder>
                     <Link to="/" decoration="none">
                         <img style={{marginRight: 8 }} src={Logo} alt="logo" />
-                        <strong>{title}</strong>
+                        <TextOverflow size="160px">
+                            <strong>{title}</strong>
+                        </TextOverflow>
                     </Link>
                 </LinkHolder>
                 {
@@ -83,18 +111,25 @@ const Header = ({ title, crumbs }) => {
                         return (crumbsLen === i + 1) ? (
                             <React.Fragment key={path}>
                                 <SeperateLine>/</SeperateLine>
-                                <SpanText>{path}</SpanText>
+                                <TextOverflow size="240px">
+                                    <SpanText>{path}</SpanText>
+                                </TextOverflow>
                             </React.Fragment>
                             ): (
                     <React.Fragment key={path}>
                         <SeperateLine>/</SeperateLine>
                         <LinkHolder>
-                            <Link to={`/${path}`} decoration="none">{path}</Link>
+                            <Link to={`/${path}`} decoration="none">
+                                <TextOverflow size="160px">
+                                    {path}
+                                </TextOverflow>
+                            </Link>
                         </LinkHolder>
                     </React.Fragment>
                     )}) : null
                 }
             </NavItemLeft>
+            <FlexSeperate />
             <NavItemRight>
                 <LinkHolder>
                     <Link to="/blog/" decoration="none">Blog</Link>
