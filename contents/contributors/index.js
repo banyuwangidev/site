70dc634438719558a7a17d969da2fe9c7c0c4154
piharpi <justharpi@gmail.com>
2020-04-02 22:58:29 +0700

    add post and contributor

--- contents/contributors/index.js
+++ contents/contributors/index.js
@@ -1,4 +1,3 @@
-
 /* silahkan menambahkan data kamu sesuai dengan format berikut
    name: <nama_kamu>
    github: <usename_github_kamu>
@@ -7,10 +6,16 @@
 */
 const contributorList = [
   {
-    name: 'Fahmi Irsyad Khairi',
-    github: 'fahmiirsyadk',
-    site: 'fahmiirsyadk.com',
-    bio: 'nothing old, and nothing boring'
+    name: "Fahmi Irsyad Khairi",
+    github: "fahmiirsyadk",
+    site: "fahmiirsyadk.com",
+    bio: "nothing old, and nothing boring",
+  },
+  {
+    name: "Mahendrata Harpi",
+    github: "piharpi",
+    site: "harpi.rocks",
+    bio: "kadang senang, dan kadang juga sedih",
   },
 ]
 module.exports = contributorList
