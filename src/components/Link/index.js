249688b4b9002558183153a9de3f3ba766512c0a
piharpi <justharpi@gmail.com>
2020-04-03 13:29:22 +0700

    Add prefix http to props external

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,31 +1,41 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
-import styled from 'styled-components'
+import * as React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
 
-const LinkStyle = styled(props => <Link {...props} />)`
-    line-height: 24px;
-    font-weight: 500;
-    text-decoration: ${props => props.decoration};
-    color: #111;
+const LinkStyle = styled((props) => <Link {...props} />)`
+  line-height: 24px;
+  font-weight: 500;
+  text-decoration: ${(props) => props.decoration};
+  color: #111;
 `
 
 const LinkExternal = styled.a`
-    line-height: 24px;
-    font-weight: 500;
-    text-decoration: ${props => props.decoration};
-    color: #111;
+  line-height: 24px;
+  font-weight: 500;
+  text-decoration: ${(props) => props.decoration};
+  color: #111;
 `
 
 LinkStyle.defaultProps = {
-    decoration: "underline"
-};
+  decoration: "underline",
+}
 
 LinkExternal.defaultProps = {
-    decoration: "underline"
-};
+  decoration: "underline",
+}
 
-const LinkComponent = (props) => props.external ? (<LinkExternal decoration={props.decoration} href={props.to} target="_blank" rel="noopener noreferrer">
-{props.children}
-</LinkExternal>) : <LinkStyle {...props} />
+const LinkComponent = (props) =>
+  props.external ? (
+    <LinkExternal
+      decoration={props.decoration}
+      href={`http://${props.to}`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {props.children}
+    </LinkExternal>
+  ) : (
+    <LinkStyle {...props} />
+  )
 
 export default LinkComponent
