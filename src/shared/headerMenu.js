2a72531357f62f24dc912e85768161b1f86ce6a5
piharpi <justharpi@gmail.com>
2020-04-03 16:50:26 +0700

    Added replace function for replacing space character with dash punctuation mark, work with url path

--- src/shared/headerMenu.js
+++ src/shared/headerMenu.js
@@ -1,3 +1,6 @@
-const menuItems = ["Blog","Event","Contributors","About"].map(item => ({title: item, path: item.toLowerCase()}))
+const menuItems = ["Blog", "Events", "Contributors", "About"].map((item) => ({
+  title: item,
+  path: item.toLowerCase().replace(/\s/gi, "-"),
+}))
 
 export default menuItems
