d5d6db83e05b2a9aa09e9b11930d75d8f515140c
fahmi irsyad khairi <fahmiirsyad10@gmail.com>
2020-04-06 00:49:50 +0700

    Add image support in markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,19 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-layout`,
       options: {
